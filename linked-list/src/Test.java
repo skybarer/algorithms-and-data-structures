54598c41d60f45c1f79a9bf1af4ff5bd606f5c6c
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2019-03-16 18:43:22 +0530

    commit linked list

--- linked-list/src/Test.java
+++ linked-list/src/Test.java
@@ -1,8 +1,11 @@
 public class Test {
+
     public static void main(String[] arr) {
         LinkedList linkedList = new LinkedList();
         linkedList.add(1);
         linkedList.add(2);
+        linkedList.add(2);
+        linkedList.add(3);
         linkedList.add(3);
         linkedList.display();
     }
