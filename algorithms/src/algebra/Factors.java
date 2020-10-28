5a59f0c8d0504d937b5357ad95c16eb8905c434d
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-10-28 20:46:13 +0530

    list out all factors of a number

---
+++ algorithms/src/algebra/Factors.java
@@ -0,0 +1,22 @@
+package algebra;
+
+import java.util.*;
+
+public class Factors {
+    public static List<Long> factors(long n) {
+        List<Long> list = new ArrayList<>();
+        for (long i = 1; i * i < n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+                list.add(n / i);
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<Long> f = factors(24);
+        Collections.sort(f);
+        System.out.println(f);
+    }
+}
